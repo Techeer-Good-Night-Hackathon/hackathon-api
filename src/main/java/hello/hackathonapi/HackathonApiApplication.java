28597cfbba1e9b0d8a2a80828f357c66a7d27894
kittae <jinkitea@naver.com>
2025-08-16 21:24:43 +0900

    chore: DevTools 의존성 추가 및 @EnableJpaAuditing 어노테이션 추가

--- src/main/java/hello/hackathonapi/HackathonApiApplication.java
+++ src/main/java/hello/hackathonapi/HackathonApiApplication.java
@@ -2,8 +2,10 @@ package hello.hackathonapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HackathonApiApplication {
 
     public static void main(String[] args) {
